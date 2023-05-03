01bdae5e7e4d448dae402062a7651f66eb3c3942
PedroSMJesus <107729385+PedroSMJesus@users.noreply.github.com>
2023-05-03 16:40:52 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import Feed from "./screens/Feed";
+import Home from "./screens/Home";
+import DrawerNavigator from "./navigation/DrawerNavigator";
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+     <Tab.Navigator
+            screenOptions={({ route }) => ({
+                tabBarIcon: ({ focused, color, size }) => {
+                    let iconName;
+                    if (route.name === 'Feed') {
+                        iconName = focused
+                            ? 'book'
+                            : 'book-outline';
+                    } else if (route.name === 'Início') {
+                        iconName = focused ? 'home' : 'home-outline';
+                    }
+                    return <Ionicons name={iconName} size={size} color={color} />;
+                },
+            })}
+            tabBarOptions={{
+                activeTintColor: 'tomato',
+                inactiveTintColor: 'gray',
+            }}
+        >
+            <Tab.Screen name="Feed" component={Feed} />
+            <Tab.Screen name="Início" component={Home} />
+        </Tab.Navigator> 
+    </NavigationContainer>
+  );
+}
+
